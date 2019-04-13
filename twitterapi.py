6c5107a28c979055351498d68de95887f750b133
evanthehacker <evchik2007@gmail.com>
2019-04-12 19:53:01 -0700

    twitterapi

---
+++ twitterapi.py
@@ -0,0 +1,45 @@
+# Import the necessary package to process data in JSON format
+# -*- coding: utf-8 -*-
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+# Import the tweepy library
+import tweepy
+
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = 'DO NOT TOUCH'
+ACCESS_SECRET = 'DO NOT TOUCH'
+CONSUMER_KEY = 'DO NOT TOUCH'
+CONSUMER_SECRET = 'DO NOT TOUCH'
+
+# Setup tweepy to authenticate with Twitter credentials:
+
+auth = tweepy.OAuthHandler("ENTER HERE", "ENTER HERE")
+auth.set_access_token("ENTER HERE", "ENTER HERE")
+
+# Create the api to connect to twitter with your creadentials
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
+#---------------------------------------------------------------------------------------------------------------------
+# wait_on_rate_limit= True;  will make the api to automatically wait for rate limits to replenish
+# wait_on_rate_limit_notify= Ture;  will make the api  to print a notification when Tweepyis waiting for rate limits to replenish
+#---------------------------------------------------------------------------------------------------------------------
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# The following loop will print most recent statuses, including retweets, posted by the authenticating user and that user’s friends.
+# This is the equivalent of timelinehome on the Web.
+#---------------------------------------------------------------------------------------------------------------------
+
+hasht = tweepy.Cursor(api.search, q='igotpizza').items(100)
+
+for tweet in hasht:
+    print(tweet.created_at, tweet.text, tweet.id)
+    api.update_status("@" + tweet.user.name  + ' Enjoy your pizza' + "        You once tweeted         " + tweet.text, tweet.id)
+
+
+#---------------------------------------------------------------------------------------------------------------------
+# Twitter API development use pagination for Iterating through timelines, user lists, direct messages, etc.
+# To help make pagination easier and Tweepy has the Cursor object.
+#---------------------------------------------------------------------------------------------------------------------
